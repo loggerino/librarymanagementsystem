a28241ef7ab081ee8f6550b947331edabac8b599
Chyisiang <chyisiang8@gmail.com>
2023-02-23 17:08:35 +0800

    issue book dates

--- src/jframe/issuebook.java
+++ src/jframe/issuebook.java
@@ -4,7 +4,17 @@
  */
 package jframe;
 
+import controller.StudentManager;
 import controller.booksDAOimpl;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import model.Student;
 import model.librarybooks;
 
 /**
@@ -19,14 +29,97 @@ public class issuebook extends javax.swing.JFrame {
     public issuebook() {
         initComponents();
     }
-    
-    public void getBookDetails(){
+
+    public void getBookDetails() {
         librarybooks books = new librarybooks();
-        
-        
+        int bookID = Integer.parseInt(txt_bookid.getText());
         booksDAOimpl dao = new booksDAOimpl();
-        
-        
+        books = dao.get(bookID);
+
+        if (books != null) {
+            lbl_bookid.setText(Integer.toString(books.getBook_id()));
+            lbl_title.setText(books.getBook_name());
+            lbl_author.setText(books.getAuthor());
+            lbl_quantity.setText(Integer.toString(books.getQuantity()));
+        } else {
+            JOptionPane.showMessageDialog(this, "Book not found.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public void getStudentDetails() {
+        Student students = new Student();
+        int studentID = Integer.parseInt(txt_studentid.getText());
+        StudentManager dao = new StudentManager();
+        students = dao.get(studentID);
+
+        if (students != null) {
+            lbl_studentid.setText(Integer.toString(students.getStudentID()));
+            lbl_name.setText(students.getName());
+            lbl_course.setText(students.getCourse());
+            lbl_faculty.setText(students.getFaculty());
+        } else {
+            JOptionPane.showMessageDialog(this, "Student not found.", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    private DateFormat[] formats = new DateFormat[]{
+        new SimpleDateFormat("yyyyMMdd"),
+        new SimpleDateFormat("dd/MM/yyyy"),
+        new SimpleDateFormat("yyyy/MM/dd"),
+        new SimpleDateFormat("dd MMM yyyy")
+    };
+
+    private Date getIssueDate() throws ParseException {
+        String issueDateStr = txt_issuedate.getText();
+        Date issueDate = null;
+        for (DateFormat format : formats) {
+            try {
+                issueDate = format.parse(issueDateStr);
+                break;
+            } catch (ParseException e) {
+                // Do nothing, try next format
+            }
+        }
+        if (issueDate == null) {
+            throw new ParseException("Unparseable date: " + issueDateStr, 0);
+        }
+        return issueDate;
+    }
+
+    private Date getReturnDate() throws ParseException {
+        String returnDateStr = txt_returndate.getText();
+        Date returnDate = null;
+        for (DateFormat format : formats) {
+            try {
+                returnDate = format.parse(returnDateStr);
+                break;
+            } catch (ParseException e) {
+                // Do nothing, try next format
+            }
+        }
+        if (returnDate == null) {
+            throw new ParseException("Unparseable date: " + returnDateStr, 0);
+        }
+        return returnDate;
+    }
+
+    public void calculateDuration() {
+        try {
+            // Get the issue and return dates
+            Date issueDate = getIssueDate();
+            Date returnDate = getReturnDate();
+
+            // Calculate the duration between the issue and return dates
+            long durationInMillis = returnDate.getTime() - issueDate.getTime();
+            long durationInDays = TimeUnit.MILLISECONDS.toDays(durationInMillis);
+
+            // Display the duration in a label or store it as necessary
+            lbl_duration.setText(Long.toString(durationInDays) + " days");
+        } catch (ParseException e) {
+            // Handle the case where the input dates are invalid
+            JOptionPane.showMessageDialog(this, "Please enter valid dates in the format yyyyMMdd, dd/MM/yyyy, yyyy/MM/dd, or dd MMM yyyy");
+            e.printStackTrace();
+        }
     }
 
     /**
@@ -65,6 +158,11 @@ public class issuebook extends javax.swing.JFrame {
         jLabel21 = new javax.swing.JLabel();
         txt_studentid = new javax.swing.JTextField();
         txt_bookid = new javax.swing.JTextField();
+        jLabel15 = new javax.swing.JLabel();
+        txt_issuedate = new javax.swing.JTextField();
+        txt_returndate = new javax.swing.JTextField();
+        jLabel23 = new javax.swing.JLabel();
+        lbl_duration = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         getContentPane().setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
@@ -204,8 +302,63 @@ public class issuebook extends javax.swing.JFrame {
         jLabel21.setForeground(new java.awt.Color(0, 51, 255));
         jLabel21.setText("Student ID");
         jPanel3.add(jLabel21, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 240, 80, 40));
+
+        txt_studentid.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txt_studentidFocusLost(evt);
+            }
+        });
         jPanel3.add(txt_studentid, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 240, 230, 40));
+
+        txt_bookid.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txt_bookidFocusLost(evt);
+            }
+        });
+        txt_bookid.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_bookidActionPerformed(evt);
+            }
+        });
         jPanel3.add(txt_bookid, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 190, 230, 40));
+
+        jLabel15.setFont(new java.awt.Font("Franklin Gothic Medium Cond", 0, 18)); // NOI18N
+        jLabel15.setForeground(new java.awt.Color(0, 51, 255));
+        jLabel15.setText("Issue Date");
+        jPanel3.add(jLabel15, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 290, 80, 40));
+
+        txt_issuedate.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txt_issuedateFocusLost(evt);
+            }
+        });
+        txt_issuedate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_issuedateActionPerformed(evt);
+            }
+        });
+        jPanel3.add(txt_issuedate, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 290, 230, 40));
+
+        txt_returndate.addFocusListener(new java.awt.event.FocusAdapter() {
+            public void focusLost(java.awt.event.FocusEvent evt) {
+                txt_returndateFocusLost(evt);
+            }
+        });
+        txt_returndate.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                txt_returndateActionPerformed(evt);
+            }
+        });
+        jPanel3.add(txt_returndate, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 340, 230, 40));
+
+        jLabel23.setFont(new java.awt.Font("Franklin Gothic Medium Cond", 0, 18)); // NOI18N
+        jLabel23.setForeground(new java.awt.Color(0, 51, 255));
+        jLabel23.setText("Return Date");
+        jPanel3.add(jLabel23, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 340, 80, 40));
+
+        lbl_duration.setFont(new java.awt.Font("Franklin Gothic Medium Cond", 0, 18)); // NOI18N
+        lbl_duration.setForeground(new java.awt.Color(0, 51, 255));
+        jPanel3.add(lbl_duration, new org.netbeans.lib.awtextra.AbsoluteConstraints(180, 400, 180, 40));
 
         getContentPane().add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(760, 0, 520, 720));
 
@@ -217,6 +370,47 @@ public class issuebook extends javax.swing.JFrame {
         Homepage.setVisible(true);
         dispose();
     }//GEN-LAST:event_jLabel1MouseClicked
+
+    private void txt_bookidActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_bookidActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_bookidActionPerformed
+
+    private void txt_bookidFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txt_bookidFocusLost
+        // TODO add your handling code here:
+        getBookDetails();
+    }//GEN-LAST:event_txt_bookidFocusLost
+
+    private void txt_studentidFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txt_studentidFocusLost
+        // TODO add your handling code here:
+        getStudentDetails();
+    }//GEN-LAST:event_txt_studentidFocusLost
+
+    private void txt_issuedateFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txt_issuedateFocusLost
+        try {
+            // TODO add your handling code here:
+            getIssueDate();
+        } catch (ParseException ex) {
+            Logger.getLogger(issuebook.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_txt_issuedateFocusLost
+
+    private void txt_issuedateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_issuedateActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_issuedateActionPerformed
+
+    private void txt_returndateFocusLost(java.awt.event.FocusEvent evt) {//GEN-FIRST:event_txt_returndateFocusLost
+        try {
+            // TODO add your handling code here:
+            getReturnDate();
+        } catch (ParseException ex) {
+            Logger.getLogger(issuebook.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        calculateDuration();
+    }//GEN-LAST:event_txt_returndateFocusLost
+
+    private void txt_returndateActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txt_returndateActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_txt_returndateActionPerformed
 
     /**
      * @param args the command line arguments
@@ -258,6 +452,7 @@ public class issuebook extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel13;
     private javax.swing.JLabel jLabel14;
+    private javax.swing.JLabel jLabel15;
     private javax.swing.JLabel jLabel16;
     private javax.swing.JLabel jLabel17;
     private javax.swing.JLabel jLabel18;
@@ -265,6 +460,7 @@ public class issuebook extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel20;
     private javax.swing.JLabel jLabel21;
     private javax.swing.JLabel jLabel22;
+    private javax.swing.JLabel jLabel23;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
@@ -274,12 +470,15 @@ public class issuebook extends javax.swing.JFrame {
     private javax.swing.JLabel lbl_author;
     private javax.swing.JLabel lbl_bookid;
     private javax.swing.JLabel lbl_course;
+    private javax.swing.JLabel lbl_duration;
     private javax.swing.JLabel lbl_faculty;
     private javax.swing.JLabel lbl_name;
     private javax.swing.JLabel lbl_quantity;
     private javax.swing.JLabel lbl_studentid;
     private javax.swing.JLabel lbl_title;
     private javax.swing.JTextField txt_bookid;
+    private javax.swing.JTextField txt_issuedate;
+    private javax.swing.JTextField txt_returndate;
     private javax.swing.JTextField txt_studentid;
     // End of variables declaration//GEN-END:variables
 }
